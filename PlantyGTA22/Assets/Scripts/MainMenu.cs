51962df189490611bf4ab1dc7dc953ee2ec319f6
Sten <bratschen01@web.de>
2022-10-28 11:54:10 +0200

    Ui update

--- PlantyGTA22/Assets/Scripts/MainMenu.cs
+++ PlantyGTA22/Assets/Scripts/MainMenu.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public AudioMixer audioMixer;
 
     public void LoadScene(string SceneName)
     {
         SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
