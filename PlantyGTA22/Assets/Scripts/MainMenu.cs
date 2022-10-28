f7eee818ce7f81113f3acce60b1e39f475c5d706
Sten <bratschen01@web.de>
2022-10-28 16:12:37 +0200

    Main Menu Update + Bug fixes

--- PlantyGTA22/Assets/Scripts/MainMenu.cs
+++ PlantyGTA22/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);    
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = resolutions.Length;
+    }
 
     public void LoadScene(string SceneName)
     {
