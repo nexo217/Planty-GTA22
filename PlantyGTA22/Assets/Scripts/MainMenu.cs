606537d1a525540d11aeff85041d6b5d9b494c7d
Sten <bratschen01@web.de>
2022-10-28 16:41:06 +0200

    UI update

--- PlantyGTA22/Assets/Scripts/MainMenu.cs
+++ PlantyGTA22/Assets/Scripts/MainMenu.cs
@@ -20,14 +20,21 @@ public class MainMenu : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for(int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);    
+            options.Add(option);  
+            
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = resolutions.Length;
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void LoadScene(string SceneName)
@@ -48,5 +55,16 @@ public class MainMenu : MonoBehaviour
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
